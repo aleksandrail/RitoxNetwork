ab971b636e50499f2d88ecf6f5ad268d9a81a1af
aleksandrail <54113006+aleksandrail@users.noreply.github.com>
2019-10-07 22:28:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-bot.jsconst Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
